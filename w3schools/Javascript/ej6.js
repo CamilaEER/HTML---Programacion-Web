0be36ced7b9e2b75b6474cead74f91c99d423faf
CamilaEER <l21070383@cdmadero.tecnm.mx>
2025-06-03 21:02:59 -0600

    lito

--- w3schools/Javascript/ej6.js
+++ w3schools/Javascript/ej6.js
@@ -1,6 +1,10 @@
-// Encontrar el número más grande en un arreglo
+// 6
+// Función para encontrar el número más grande en un arreglo
 function numeroMayor(arr) {
+  // Usamos Math.max con el spread operator para pasar los elementos como argumentos
   return Math.max(...arr);
 }
 
+// Ejemplo de arreglo con números
 console.log(numeroMayor([3, 7, 2, 99, 4])); // 99
+
